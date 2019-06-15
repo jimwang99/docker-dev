4c2ca1cf8f45147a8a405bfa7572073985561c27
Jim Wang <phdbreak99@gmail.com>
2019-06-15 15:14:26 -0700

    for DejaGNU

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ USER 0
 RUN apt-get update
 RUN apt-get install -y vim wget python python3 gcc g++ make git
 RUN apt-get install -y gvim gedit
+RUN apt-get install -y apt-file
+RUN apt-file update
 
 RUN ln -sf /mnt/host ~/docker
 RUN echo "if [[ -f ~/docker/dev/_bashrc ]]; then source ~/docker/dev/_bashrc; else echo 'no _bashrc'; fi" >> ~/.bashrc
--- riscv.sh/riscv-gnu-toolchain/00-prepare.sh
+++ riscv.sh/riscv-gnu-toolchain/00-prepare.sh
@@ -1,3 +1,3 @@
 # filename: ~/docker/dev/riscv.sh/riscv-gnu-toolchain/00-prepare.sh
 apt-get install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
-apt-get install -y build-essential zlib1g-dev pkg-config libglib2.0-dev binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython-dev python-pip python-capstone virtualenv tk-dev
+apt-get install -y build-essential zlib1g-dev pkg-config libglib2.0-dev binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython-dev python-pip python-capstone virtualenv expect
